537debbffe8a53f826ddf4011afcd7913d9895bf
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-03-17 16:18:40 +0100

    Method to get the Fetched Access Token

--- app/api/concerns/api_guard.rb
+++ app/api/concerns/api_guard.rb
@@ -17,6 +17,13 @@
  
    	# Helper Methods for Grape Endpoint
     module HelperMethods
+    	private
+		def get_token_string
+	        # The token was stored after the authenticator was invoked.
+	        # It could be nil. The authenticator does not check its existence.
+	        request.env[Rack::OAuth2::Server::Resource::ACCESS_TOKEN]
+ 	    end
+
     end
  
     module ClassMethods
