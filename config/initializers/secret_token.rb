780c9bc7717771c2b821f674300deec94677893d
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-03-17 11:45:19 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ByewalletApi::Application.config.secret_key_base = 'bb7821f11592cbe5ba052ce9949c9510b6992f39f766371013d8c72561be5b2358e54d6d30c146403398619c63c8ca5de72a160db71cbdab3c4a6058b4a50665'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ByewalletApi::Application.config.session_store :cookie_store, key: '_byewallet-api_session'
